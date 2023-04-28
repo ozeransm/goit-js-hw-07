7589d4830d7b9df704d19e55bf69ee53c3242966
Serhii Ozeran <ozeransm@gmail.com>
2023-04-28 21:23:27 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const imagesGallery = galleryItems.reduce((img,{preview,description,original})=>img+=`<li class="gallery__item"><a class ="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" title="${description}"></a></li>`,'');
+gallery.insertAdjacentHTML('afterbegin', imagesGallery);
+// console.log(imagesGallery);
+
+new SimpleLightbox('.gallery a',{captionDelay:250});
